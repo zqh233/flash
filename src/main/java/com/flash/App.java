ea0e7d7c7819f00a63c7e59ecce86be84d043225
zhuqianhui <qianhui.zhu@baitao.com>
2019-01-16 16:50:24 +0800

    first commit

---
+++ src/main/java/com/flash/App.java
@@ -0,0 +1,25 @@
+package com.flash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableAutoConfiguration
+@RestController
+public class App {
+
+    @RequestMapping("/")
+    public String home() {
+        return "hello world!";
+    }
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
+    }
+}
