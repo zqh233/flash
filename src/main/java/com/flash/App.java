3649aabee764d3f16f75280c44a54caf8bea83b5
zhuqianhui <qianhui.zhu@baitao.com>
2019-01-16 17:15:33 +0800

    整合mybatis

--- src/main/java/com/flash/App.java
+++ src/main/java/com/flash/App.java
@@ -1,7 +1,12 @@
 package com.flash;
 
+import com.flash.dao.UserDOMapper;
+import com.flash.dataObject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
  * Hello world!
  *
  */
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"com.flash"})
 @RestController
+@MapperScan("com.flash.dao")
 public class App {
+
+    @Autowired
+    private UserDOMapper userDOMapper;
 
     @RequestMapping("/")
     public String home() {
-        return "hello world!";
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if(userDO == null) {
+            return "用户对象不存在";
+        } else {
+            return userDO.getName();
+        }
     }
 
     public static void main( String[] args ) {
