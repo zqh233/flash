96099ff7c860a4f98d5afa2ba1b530512b3a0f11
zqh233 <237668461@qq.com>
2019-01-17 16:01:05 +0800

    test mac

--- src/main/java/com/flash/App.java
+++ src/main/java/com/flash/App.java
@@ -32,6 +32,7 @@ public class App {
         }
     }
 
+
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         SpringApplication.run(App.class, args);
