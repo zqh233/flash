74f95f8ec8c31f70a6d43b9984a2d1bea15d5ff1
zqh233 <237668461@qq.com>
2019-01-24 14:18:24 +0800

    item

---
+++ src/main/java/com/flash/service/model/itemModel.java
@@ -0,0 +1,98 @@
+package com.flash.service.model;
+
+import java.math.BigDecimal;
+
+public class itemModel {
+
+    private Integer id;
+
+    /**
+     * 商品名称
+     */
+    private String name;
+
+    /**
+     * 商品价格
+     */
+    private BigDecimal price;
+
+    /**
+     * 商品库存
+     */
+    private Integer stock;
+
+    /**
+     * 商品描述
+     */
+    private String description;
+
+    /**
+     * 销量
+     */
+    private Integer sales;
+
+    /**
+     * 商品图片
+     */
+    private String imgUrl;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getSales() {
+        return sales;
+    }
+
+    public void setSales(Integer sales) {
+        this.sales = sales;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
+
+
+
