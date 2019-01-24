442ab231db53d005d6057739fb7a3a48f3b6eda5
zhuqianhui <qianhui.zhu@baitao.com>
2019-01-24 15:55:42 +0800

    解决汉子乱码

--- src/main/java/com/flash/controller/ItemController.java
+++ src/main/java/com/flash/controller/ItemController.java
@@ -1,4 +1,45 @@
 package com.flash.controller;
 
+import com.alibaba.druid.sql.ast.expr.SQLCaseExpr;
+import com.flash.Response.CommonReturn;
+import com.flash.controller.viewObject.ItemVO;
+import com.flash.service.ItemService;
+import com.flash.service.model.ItemModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.math.BigDecimal;
+
+@Controller("item")
+@RequestMapping("/item")
+@CrossOrigin(allowCredentials = "true", allowedHeaders = "*")
 public class ItemController extends BaseController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @RequestMapping(value = "/create", method = {RequestMethod.POST}, consumes = {CONTENT_TYPE_FORMED})
+    @ResponseBody
+    public CommonReturn createItem(@RequestParam(name = "title") String title,
+                                   @RequestParam(name = "description") String description,
+                                   @RequestParam(name = "stock") Integer stock,
+                                   @RequestParam(name = "price")BigDecimal price,
+                                   @RequestParam(name = "imgUrl") String imgUrl) throws Exception {
+        //创建商品model
+        ItemModel itemModel = new ItemModel();
+        itemModel.setName(title);
+        itemModel.setPrice(price);
+        itemModel.setStock(stock);
+        itemModel.setDescription(description);
+        itemModel.setImgUrl(imgUrl);
+
+        ItemModel model = itemService.createItem(itemModel);
+
+        return CommonReturn.create(ItemVO.convertFromModel(model));
+    }
 }
--- src/main/java/com/flash/controller/viewObject/ItemVO.java
+++ src/main/java/com/flash/controller/viewObject/ItemVO.java
@@ -1,4 +1,112 @@
 package com.flash.controller.viewObject;
 
+import com.flash.dataObject.ItemDO;
+import com.flash.dataObject.ItemStockDO;
+import com.flash.service.model.ItemModel;
+import org.springframework.beans.BeanUtils;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
 public class ItemVO {
+
+    private Integer id;
+
+    /**
+     * 商品名称
+     */
+    private String name;
+
+    /**
+     * 商品价格
+     */
+    private BigDecimal price;
+
+    /**
+     * 商品库存
+     */
+    private Integer stock;
+
+    /**
+     * 商品描述
+     */
+    private String description;
+
+    /**
+     * 销量
+     */
+    private Integer sales;
+
+    /**
+     * 商品图片
+     */
+    private String imgUrl;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getSales() {
+        return sales;
+    }
+
+    public void setSales(Integer sales) {
+        this.sales = sales;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public static ItemVO convertFromModel(ItemModel itemModel) {
+        if (itemModel == null) {
+            return null;
+        }
+        ItemVO itemVO = new ItemVO();
+        BeanUtils.copyProperties(itemModel, itemVO);
+        return itemVO;
+    }
 }
